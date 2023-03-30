86dd86e5345c7f3e7a8bfc37bc24512ef715bd08
gianglinhg <giangvanlinhbq89@gmail.com>
2023-03-30 12:36:15 +0700

    upload thêm folder

--- resources/views/client/layouts/navbar.blade.php
+++ resources/views/client/layouts/navbar.blade.php
@@ -7,12 +7,12 @@
        </div>
 
        <ul class="list-unstyled components">
-           
-           <li class="active"> <a href="index.html">Home</a></li>
-            <li> <a href="about.html">About</a></li>
-                                   <li> <a href="product.html">Product</a></li>
-                                   <li> <a href="blog.html">Blog</a></li>
-                                   <li> <a href="contact.html">Contact us</a></li>
+
+<li class="active"> <a href="index.html">Home</a></li>
+<li> <a href="about.html">About</a></li>  
+<li> <a href="product.html">Product</a></li>
+<li> <a href="blog.html">Blog</a></li>
+<li> <a href="contact.html">Contact us</a></li>
 
        </ul>
 
